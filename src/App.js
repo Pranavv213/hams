63e65bac5104cb1fd9b6c1fab09f0cbbf52abd6e
new <new@Pranavs-Mac-mini.local>
2024-07-02 00:01:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import Circle from './Circle'
 function App() {
-  const [tapCount, setTapCount] = useState(0);
-  const [tapHandled, setTapHandled] = useState(false);
-  const [file, setFile] = useState();
 
+  const [file,setFile]=useState()
   function handleChange(e) {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
 }
 
-  const handleMultiTap = (event) => {
-    const tapArea = document.getElementById('tap-area');
-
-    if (event.type === 'click') {
-      if (!tapHandled) {
-        createTapFeedback(event.clientX, event.clientY, tapArea);
-        updateTapCount();
-      }
-      setTapHandled(false);
-    } else {
-      for (let i = 0; i < event.changedTouches.length; i++) {
-        const touch = event.changedTouches[i];
-        createTapFeedback(touch.clientX, touch.clientY, tapArea);
-        updateTapCount();
-      }
-      setTapHandled(true);
-    }
-  };
-
-  const createTapFeedback = (x, y, tapArea) => {
-    const tapFeedback = document.createElement('div');
-    tapFeedback.className = 'tap-feedback';
-    tapFeedback.textContent = '+1';
-    tapFeedback.style.left = `${x - tapArea.getBoundingClientRect().left}px`;
-    tapFeedback.style.top = `${y - tapArea.getBoundingClientRect().top}px`;
-
-    tapArea.appendChild(tapFeedback);
-
-    setTimeout(() => {
-      tapFeedback.remove();
-    }, 1000);
-  };
-
-  const updateTapCount = () => {
-    setTapCount(prevCount => prevCount + 1);
-  };
-
   return (
     <div className="App">
-      <div id="game-area">
-        <div id="tap-count">Taps: {tapCount}</div>
-        <div id="tap-area" onClick={handleMultiTap} onTouchStart={handleMultiTap}>
-         
+      <br></br>
+      <br></br>
+      <div class="nav" style={{width:'100%'}}> <input type="file" id="file" class="file-input" onChange={handleChange}/>
+      <label for="file" class="custom-file-label">Choose Image</label>
 
-            {file==null &&  <input type="file" onChange={handleChange} /> }
-
-         
-          <img style={{width:'300px', height:'300px',borderRadius:'50%'}} src={file} />
-          
-        </div>
+      &nbsp;
+      
+      
       </div>
+
+     
+      <Circle img={file}/>
     </div>
   );
 }
---
+++ src/Circle.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './Circle.css';
+
+function Circle({img}) {
+  const [tapCount, setTapCount] = useState(0);
+  const [tapHandled, setTapHandled] = useState(false);
+
+
+
+
+  const handleMultiTap = (event) => {
+    const tapArea = document.getElementById('tap-area');
+
+    if (event.type === 'click') {
+      if (!tapHandled) {
+        createTapFeedback(event.clientX, event.clientY, tapArea);
+        updateTapCount();
+      }
+      setTapHandled(false);
+    } else {
+      for (let i = 0; i < event.changedTouches.length; i++) {
+        const touch = event.changedTouches[i];
+        createTapFeedback(touch.clientX, touch.clientY, tapArea);
+        updateTapCount();
+      }
+      setTapHandled(true);
+    }
+  };
+
+  const createTapFeedback = (x, y, tapArea) => {
+    const tapFeedback = document.createElement('div');
+    tapFeedback.className = 'tap-feedback';
+    tapFeedback.textContent = '+1';
+    tapFeedback.style.left = `${x - tapArea.getBoundingClientRect().left}px`;
+    tapFeedback.style.top = `${y - tapArea.getBoundingClientRect().top}px`;
+
+    tapArea.appendChild(tapFeedback);
+
+    setTimeout(() => {
+      tapFeedback.remove();
+    }, 1000);
+  };
+
+  const updateTapCount = () => {
+    setTapCount(prevCount => prevCount + 1);
+  };
+
+  return (
+    <div className="App">
+
+      
+     
+      <div >
+     
+     <center>
+        <div id="tap-count">{tapCount}</div>
+        </center>
+        <div id="tap-area" onClick={handleMultiTap} onTouchStart={handleMultiTap}>
+    
+        <img style={{height:'300px',width:'300px',borderRadius:'50%'}}src={img}></img>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Circle;
