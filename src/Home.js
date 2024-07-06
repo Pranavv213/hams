5da4863607264ecfb0f4515aa50b750c1bce988b
new <new@Pranavs-Mac-mini.local>
2024-07-06 07:59:33 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -95,8 +95,9 @@ const App = () => {
         sendEmailVerification(auth.currentUser).then((data2)=>{
           // alert('Email Verification Link Sent')
           setShow(false)
+          handleClick()
           // setTimeout(()=>{
-          //   handleClick()
+          
           // },500)
           
           // setTimeout(()=>{
